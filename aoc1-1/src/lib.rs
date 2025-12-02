d505c70c6ce9ab827b4eb86ae96366c4bb8d0da8
Andrew-McCall <andrewdavidmccall@hotmail.com>
2025-12-02 10:08:45 +0000

    5876

--- aoc1-1/src/lib.rs
+++ aoc1-1/src/lib.rs
@@ -1,8 +1,6 @@
 use std::fmt;
 
 pub fn solution(input_path: &str) -> Result<Answer, String> {
-    println!("Hello, world!");
-
     let mut answer = Answer {
         final_number: 50,
         zero_count: 0,
@@ -19,29 +17,31 @@ pub fn solution(input_path: &str) -> Result<Answer, String> {
         )
     })?;
 
+    // let mut debug = 0;
     for line in input.lines() {
+        // debug += 1;
+        // if debug > 20 {
+        //     break;
+        // }
         let (start, value) = parse_line(line)?;
 
+        let prev = answer.final_number;
         match start {
             'R' => answer.final_number += value,
             'L' => answer.final_number -= value,
             _ => return Err(format!("Invalid Line (start): {line}")),
         }
 
-        let new_zero_passes = answer.final_number / 100;
-        answer.zero_passes += (new_zero_passes - 1).abs();
-        answer.final_number += 100;
-        answer.final_number %= 100;
-
-        print!(
-            "{line} {} ({})",
-            answer.final_number,
-            (new_zero_passes - 1).abs()
-        );
+        let prev_q = prev.div_euclid(100);
+        let new_q = answer.final_number.div_euclid(100);
+        answer.zero_passes += (new_q - prev_q).abs();
+        answer.final_number = answer.final_number.rem_euclid(100);
 
         if answer.final_number == 0 {
             answer.zero_count += 1;
         }
+
+        println!("{} {}", answer.final_number, answer.zero_passes);
     }
 
     Ok(answer)
@@ -76,5 +76,56 @@ impl fmt::Display for Answer {
             "{} {} ({})",
             self.zero_count, self.zero_passes, self.final_number
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(parse_line("R100").unwrap(), ('R', 100));
+        assert_eq!(parse_line("L100").unwrap(), ('L', 100));
+    }
+
+    #[test]
+    fn test_div_euclid() {
+        let mut counter: i32;
+        counter = 100;
+        counter = counter.div_euclid(100);
+        assert_eq!(counter, 1);
+
+        counter = -250;
+        counter = counter.div_euclid(100);
+        assert_eq!(counter, -3);
+
+        counter = -25;
+        counter = counter.div_euclid(100);
+        assert_eq!(counter, -1);
+    }
+
+    #[test]
+    fn test_rem_euclid() {
+        let mut counter: i32;
+        counter = 100;
+        counter = counter.rem_euclid(100);
+        assert_eq!(counter, 0);
+
+        counter = 99;
+        counter = counter.rem_euclid(100);
+        assert_eq!(counter, 99);
+
+        counter = 102;
+        counter = counter.rem_euclid(100);
+        assert_eq!(counter, 2);
+
+        counter = -100;
+        counter = counter.rem_euclid(100);
+        assert_eq!(counter, 0);
+
+        counter = -51;
+        counter = counter.rem_euclid(100);
+        assert_eq!(counter, 49);
     }
 }
